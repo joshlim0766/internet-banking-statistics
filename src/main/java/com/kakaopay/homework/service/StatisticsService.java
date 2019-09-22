50f9775f6f5e9ba0c0663adb5e4f2299e7fb4c84
Jaeseung Lim <unixwizard@naver.com>
2019-09-22 21:22:04 +0900

    Fixed forecast bug.

--- src/main/java/com/kakaopay/homework/service/StatisticsService.java
+++ src/main/java/com/kakaopay/homework/service/StatisticsService.java
@@ -162,31 +162,47 @@ public class StatisticsService {
 
         int population = 10000000;
 
+        // 2019년 인구 예측
         double[] populations = forecastSources
                 .stream()
                 .mapToDouble(dto -> (dto.getOverallRate() / 100 * population)).toArray();
         DoubleExponentialSmoothingForLinearSeries.Model populationModel =
-                DoubleExponentialSmoothingForLinearSeries.fit(populations, 0.995, 0.8);
-        double[] populationForcsated = populationModel.forecast(1);
-        if (populationForcsated == null || populationForcsated.length < 1) {
+                DoubleExponentialSmoothingForLinearSeries.fit(populations, 0.9, 0.001);
+        double[] result = populationModel.forecast(1);
+        if (result == null || result.length < 1) {
             throw new RuntimeException("Couldn't forecast.");
         }
+
+        int forecastedOverallPopulation = (int) result[0];
 
         double[] datas = forecastSources
                 .stream()
                 .mapToDouble(dto -> (dto.getOverallRate() / 100 * population) * (dto.getRate() / 100)).toArray();
 
+        // 엑셀로 미리 구해둔 alpha 값 활용
+        Map<String, Double> alphaMap = new HashMap<>();
+        alphaMap.put("스마트폰", 0.25);
+        alphaMap.put("데스크탑 컴퓨터", 0.75);
+        alphaMap.put("노트북 컴퓨터", 0.002);
+        alphaMap.put("기타", 0.5);
+        alphaMap.put("스마트패드", 0.25);
+
         DoubleExponentialSmoothingForLinearSeries.Model model =
-                DoubleExponentialSmoothingForLinearSeries.fit(datas, 0.995, 0.8);
-        double[] forecasted = model.forecast(1);
-        if (forecasted == null || forecasted.length < 1) {
+                DoubleExponentialSmoothingForLinearSeries.fit(
+                        datas, alphaMap.get(forecastSources.get(0).getDeviceName()), 0.001);
+        result = model.forecast(1);
+        if (result == null || result.length < 1) {
             throw new RuntimeException("Couldn't forecast.");
         }
+
+        int forecastedPopulation = (int) result[0];
 
         StatisticsDTO statisticsDTO = new StatisticsDTO();
         statisticsDTO.setYear((short) 2019);
         statisticsDTO.setDeviceName(forecastSources.get(0).getDeviceName());
-        statisticsDTO.setRate(Math.round((forecasted[0] / populationForcsated[0] * 1000)) / 10.0);
+        double forecastedRate = ((double) forecastedPopulation / (double) forecastedOverallPopulation) * 100;
+        if (forecastedRate < 0) forecastedRate = 0.0;
+        statisticsDTO.setRate( Math.round(forecastedRate * 100) / 100.0);
 
         StatisticsResponse response = new StatisticsResponse();
 
